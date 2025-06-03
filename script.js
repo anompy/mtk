95d292fea2b5dd403424045090329ae88b66419c
Anompy <87983066+anompy@users.noreply.github.com>
2025-06-03 09:37:50 +0700

    update script.js

--- script.js
+++ script.js
@@ -251,7 +251,7 @@ window.addEventListener('load', (event) => {
     
     telegram.CloudStorage.getItem('math_high_score', (error, value) => {
         highScore = value;
-        highScoreElement.textContent = highScore;
+        highScoreElement.textContent = highScore || 0;
     });
 
     bindEvent();
